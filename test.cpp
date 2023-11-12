db3e10ed03bb6852ba1f9b886fd00c2443610fda
Amberisfree <lavamber@gmail.com>
2023-11-12 19:44:47 +0000

    GridSizeTest added in test.cpp

--- test.cpp
+++ test.cpp
@@ -8,6 +8,19 @@ TEST(TypeMatch, CheckTypeMatch) {
     EXPECT_EQ(typeid(k), typeid(v[0][0])) << "The types of k and v are not match";
 }
 
+TEST(GridSizeTest, SameSize) {
+    bool rows = (u.size(), v.size());
+
+    // If the number of rows is the same, check each row's size is right.
+    if(rows)
+    {
+        for (size_t i = 0; i < u.size(); ++i) {
+            EXPECT_EQ(u[i].size(), v[i].size()) << "Row" << i << "has different sizes in u and v.";
+        }
+    }
+}
+
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
