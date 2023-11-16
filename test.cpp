921917970072d7ef61b3bd26d569ce7611ca54cb
Raymond.L <93538228+liaoh1@users.noreply.github.com>
2023-11-16 20:15:36 +0000

    Update test.cpp
    There is an issue in second test, the bool condition test doesn' right actually.

--- test.cpp
+++ test.cpp
@@ -4,37 +4,32 @@
 
 // Check that the type of the model parameters (F, k) matches that of the element type of the u and v vectors.
 TEST(TypeMatch, CheckTypeMatch) {
+    init();
     EXPECT_EQ(typeid(F), typeid(u[0][0])) << "The types of F and u are not match";
     EXPECT_EQ(typeid(k), typeid(v[0][0])) << "The types of k and v are not match";
 }
 
 TEST(GridSizeTest, SameSize) {
-    bool rows = (u.size(), v.size());
+    init();
+    EXPECT_EQ(u.size(), v.size()) << "Col of u and v has different size";
 
-    // If the number of rows is the same, check each row's size is right.
-    if(rows)
-    {
-        for (size_t i = 0; i < u.size(); ++i) {
-            EXPECT_EQ(u[i].size(), v[i].size()) << "Row" << i << "has different sizes in u and v.";
-        }
+    for (size_t i = 0; i < u.size(); ++i) {
+        EXPECT_EQ(u[i].size(), v[i].size()) << "Row of u and v" << i << "has different size";
     }
 }
 
 TEST(SimulationTest, HandlesZeroInput) {
-    const int width = 256;
-    const int height = 256;
-    const double dt = 0.06;
-    std::vector<std::vector<double>> u, v;
+    init();
+    std::vector<std::vector<double>>u(width, std::vector<double>(height, 0.0));
+    std::vector<std::vector<double>>v(width, std::vector<double>(height, 0.0));
 
-    u = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
-    v = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
 
     // Run the simulation
     simulateStep();
 
     // Check that u and v are still zero after simulation
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
             EXPECT_DOUBLE_EQ(0.0, u[i][j]) << "u[" << i << "][" << j << "] is not zero.";
             EXPECT_DOUBLE_EQ(0.0, v[i][j]) << "v[" << i << "][" << j << "] is not zero.";
         }
